903c325f498b3fbb0d4666a8dc96f92cfb3eace0
bigtree <zz1z2zz3z4zz5@qq.com>
2024-04-19 17:38:59 +0800

    第25次保存，修复页面适配问题

--- vue.config.js
+++ vue.config.js
@@ -8,56 +8,56 @@ module.exports = {
     port: 80,
     proxy: {
       '/servers': {
-        target: 'http://localhost:9090/servers',
+        target: 'http://47.96.120.46:9090/servers',
         changeOrigin: true,
         pathRewrite: {
           '^/servers': '',
         },
       },
       '/players': {
-        target: 'http://localhost:9090/players',
+        target: 'http://47.96.120.46:9090/players',
         changeOrigin: true,
         pathRewrite: {
           '^/players': '',
         },
       },
       '/signin': {
-        target: 'http://localhost:9091/signin',
+        target: 'http://47.96.120.46:9091/signin',
         changeOrigin: true,
         pathRewrite: {
           '^/signin': '',
         },
       },
       '/signup': {
-        target: 'http://localhost:9091/signup',
+        target: 'http://47.96.120.46:9091/signup',
         changeOrigin: true,
         pathRewrite: {
           '^/signup': '',
         },
       },
       '/genCaptcha': {
-        target: 'http://localhost:9091/genCaptcha',
+        target: 'http://47.96.120.46:9091/genCaptcha',
         changeOrigin: true,
         pathRewrite: {
           '^/genCaptcha': '',
         },
       },
       '/verifyCaptcha': {
-        target: 'http://localhost:9091/verifyCaptcha',
+        target: 'http://47.96.120.46:9091/verifyCaptcha',
         changeOrigin: true,
         pathRewrite: {
           '^/verifyCaptcha': '',
         },
       },
       '/allplayers': {
-        target: 'http://localhost:9091/allplayers',
+        target: 'http://47.96.120.46:9091/allplayers',
         changeOrigin: true,
         pathRewrite: {
           '^/allplayers': '',
         },
       },
       '/recentmatch': {
-        target: 'http://localhost:9091/recentmatch',
+        target: 'http://47.96.120.46:9091/recentmatch',
         changeOrigin: true,
         pathRewrite: {
           '^/recentmatch': '',
